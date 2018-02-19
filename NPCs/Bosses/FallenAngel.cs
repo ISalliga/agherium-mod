afaa071c17c8b67ee335c8b2606f371f7063c5ef
Turquoise <reezzza.ai@gmail.com>
2018-02-19 08:34:30 -0800

    Add files via upload

--- NPCs/Bosses/FallenAngel.cs
+++ NPCs/Bosses/FallenAngel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria;
+using AgheriumMod;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -14,10 +15,14 @@ namespace AgheriumMod.NPCs.Bosses
     [AutoloadBossHead]
     public class FallenAngel : ModNPC
     {
-        public int phase = 0;
-        int despawn = 10;
-        bool saidPhase2 = false;
-        int phasechangetime = 0;
+        public int phase = 1;
+        public int despawn = 10;
+		public int teleportTime = 0;
+		public int laserSpreadTime = 0;
+		public int laserSpreadChance = 0;
+		public int laserOrbTime = 0;
+        public bool saidPhase2 = false;
+		public bool saidPhase3 = false;
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("The Fallen Angel");
@@ -28,7 +33,7 @@ namespace AgheriumMod.NPCs.Bosses
             npc.width = 24;
             npc.height = 48;
             npc.damage = 37;
-            npc.defense = 14;
+            npc.defense = 8;
             npc.lifeMax = 19800;
             npc.HitSound = SoundID.NPCHit4;
             npc.DeathSound = SoundID.NPCDeath1;
@@ -64,7 +69,6 @@ namespace AgheriumMod.NPCs.Bosses
         }
         public override void AI()
         {
-            phasechangetime++;
             if (Main.player[npc.target].dead || !Main.player[npc.target].active)
             {
                 despawn--;
@@ -73,18 +77,27 @@ namespace AgheriumMod.NPCs.Bosses
             {
                 npc.active = false;
             }
-            if (phasechangetime >= 370)
+            if (npc.life <= npc.lifeMax * 0.75f)
             {
-                phasechangetime = 0;
-            }
-            if (npc.life <= npc.lifeMax * 0.5f)
-            {
-                phase++;
+                phase = 2;
                 if (!saidPhase2)
                 {
                     saidPhase2 = true;
                     {
-                        
+                        Main.NewText("Oh, but we're just getting started!", 255, 0, 0);
+                    }
+                }
+            }
+			if (npc.life <= npc.lifeMax * 0.35f)
+            {
+                phase = 3;
+                if (!saidPhase3)
+                {
+                    saidPhase3 = true;
+                    {
+                        Main.NewText("Think you've got me all figured out, eh? Well say hi to my robot clones for me!", 255, 0, 0);
+						NPC.NewNPC((int)npc.Center.X - Main.rand.Next(-250, 250), (int)npc.Center.Y - Main.rand.Next(-250, 250), mod.NPCType("AngelClone"));
+						NPC.NewNPC((int)npc.Center.X - Main.rand.Next(-250, 250), (int)npc.Center.Y - Main.rand.Next(-250, 250), mod.NPCType("AngelClone2"));
                     }
                 }
             }
@@ -99,55 +112,67 @@ namespace AgheriumMod.NPCs.Bosses
             toTarget.Normalize();
             npc.velocity = toTarget * maxSpeed;
             npc.velocity = toTarget * maxSpeed;
-            if (phase <= 1)
-            {
-                if (npc.target < 0 || npc.target == 255 || Main.player[npc.target].dead || !Main.player[npc.target].active)
-                {
-                    npc.TargetClosest(true);
-                }
-                npc.netUpdate = true;
-                if (phasechangetime == 120 || phasechangetime == 240 || phasechangetime == 360)
-                {
-                    if (Main.rand.Next(30) == 0)
-                    {
-                        
-                    }
-                    Vector2 value9 = new Vector2(npc.position.X + (float)npc.width * 0.5f, npc.position.Y + (float)npc.height * 0.5f);
+			
+			if (phase == 1)
+			{
+				laserSpreadTime++;
+				laserSpreadChance = Main.rand.Next(0, 4);
+				if (laserSpreadTime >= 70)
+				{
+					if (laserSpreadChance == 1)
+					{
+						Vector2 value9 = new Vector2(npc.position.X + (float)npc.width * 0.5f, npc.position.Y + (float)npc.height * 0.5f);
                     float spread = 45f * 0.0174f;
                     double startAngle = Math.Atan2(npc.velocity.X, npc.velocity.Y) - spread / 2;
                     double deltaAngle = spread / 8f;
                     double offsetAngle;
-                    int damage = 20;
+                    int damage = 36;
                     int projectileShot = 100;
                     int i;
                     for (i = 0; i < 7; i++)
+						{
+							offsetAngle = (startAngle + deltaAngle * (i + i * i) / 2f) + 32f * i;
+							Projectile.NewProjectile(value9.X, value9.Y, (float)(Math.Sin(offsetAngle) * 5f), (float)(Math.Cos(offsetAngle) * 5f), projectileShot, damage, 0f, Main.myPlayer, 0f, 0f);
+							Projectile.NewProjectile(value9.X, value9.Y, (float)(-Math.Sin(offsetAngle) * 5f), (float)(-Math.Cos(offsetAngle) * 5f), projectileShot, damage, 0f, Main.myPlayer, 0f, 0f);
+						}
+					}
+					laserSpreadTime = 0;
+				}
+			}
+			
+			if (phase >= 2)
+			{
+				teleportTime++;
+				if (teleportTime >= 240)
+				{
+					Vector2 value9 = new Vector2(npc.position.X + (float)npc.width * 0.5f, npc.position.Y + (float)npc.height * 0.5f);
+                    float spread = 45f * 0.0174f;
+                    double startAngle = Math.Atan2(npc.velocity.X, npc.velocity.Y) - spread / 2;
+                    double deltaAngle = spread / 8f;
+                    double offsetAngle;
+                    int damage = 36;
+                    int projectileShot = 100;
+                    int i;
+                    for (i = 0; i < 7; i++)
+					{
+						offsetAngle = (startAngle + deltaAngle * (i + i * i) / 2f) + 32f * i;
+						Projectile.NewProjectile(value9.X, value9.Y, (float)(Math.Sin(offsetAngle) * 5f), (float)(Math.Cos(offsetAngle) * 5f), projectileShot, damage, 0f, Main.myPlayer, 0f, 0f);
+						Projectile.NewProjectile(value9.X, value9.Y, (float)(-Math.Sin(offsetAngle) * 5f), (float)(-Math.Cos(offsetAngle) * 5f), projectileShot, damage, 0f, Main.myPlayer, 0f, 0f);
+					}
+					npc.position.X = Main.player[npc.target].position.X - Main.rand.Next(-250, 250);
+					npc.position.Y = Main.player[npc.target].position.Y - Main.rand.Next(-250, 250);
+					teleportTime = 0;
+				}
+			}
+			
+			laserOrbTime++;
+				if (laserOrbTime >= 180)
+				{
+					for (int k = 0; k < 3; k++)
                     {
-                        offsetAngle = (startAngle + deltaAngle * (i + i * i) / 2f) + 32f * i;
-                        Projectile.NewProjectile(value9.X, value9.Y, (float)(Math.Sin(offsetAngle) * 5f), (float)(Math.Cos(offsetAngle) * 5f), projectileShot, damage, 0f, Main.myPlayer, 0f, 0f);
-                        Projectile.NewProjectile(value9.X, value9.Y, (float)(-Math.Sin(offsetAngle) * 5f), (float)(-Math.Cos(offsetAngle) * 5f), projectileShot, damage, 0f, Main.myPlayer, 0f, 0f);
-                    }
-                }
-                if (phasechangetime == 150)
-                {
-                    for (int k = 0; k < 3; k++)
-                    {
-                        float Speed = 3.5f;
+                        float Speed = 6.5f;
                         Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
-                        int damage = 20;
-                        int type = mod.ProjectileType("MiniArchorb");
-                        float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
-                        int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, Main.myPlayer);
-                        Main.projectile[num54].velocity.X += (float)Main.rand.Next(-20, 21) * 0.05f;
-                        Main.projectile[num54].velocity.Y += (float)Main.rand.Next(-20, 21) * 0.05f;
-                    }
-                }
-                if (phasechangetime == 155)
-                {
-                    for (int k = 0; k < 3; k++)
-                    {
-                        float Speed = 3.5f;
-                        Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
-                        int damage = 20;
+                        int damage = 46;
                         int type = mod.ProjectileType("MiniArchorb");
                         float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
                         int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, Main.myPlayer);
@@ -155,109 +180,8 @@ namespace AgheriumMod.NPCs.Bosses
                         Main.projectile[num54].velocity.Y += (float)Main.rand.Next(-20, 21) * 0.05f;
                         Main.projectile[num54].netUpdate = true;
                     }
-                }
-                if (phasechangetime == 160)
-                {
-                    for (int k = 0; k < 3; k++)
-                    {
-                        float Speed = 3.5f;
-                        Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
-                        int damage = 20;
-                        int type = mod.ProjectileType("MiniArchorb");
-                        float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
-                        int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, Main.myPlayer);
-                        Main.projectile[num54].velocity.X += (float)Main.rand.Next(-20, 21) * 0.05f;
-                        Main.projectile[num54].velocity.Y += (float)Main.rand.Next(-20, 21) * 0.05f;
-                        Main.projectile[num54].netUpdate = true;
-                    }
-                }
-            }
-            else
-            {
-                {
-                    if (npc.target < 0 || npc.target == 255 || Main.player[npc.target].dead || !Main.player[npc.target].active)
-                    {
-                        npc.TargetClosest(true);
-                    }
-                    npc.netUpdate = true;
-                    if (phasechangetime == 90 || phasechangetime == 180 || phasechangetime == 270 || phasechangetime == 360)
-                    {
-                        Vector2 value9 = new Vector2(npc.position.X + (float)npc.width * 0.5f, npc.position.Y + (float)npc.height * 0.5f);
-                        float spread = 45f * 0.0174f;
-                        double startAngle = Math.Atan2(npc.velocity.X, npc.velocity.Y) - spread / 2;
-                        double deltaAngle = spread / 8f;
-                        double offsetAngle;
-                        int damage = 20;
-                        int projectileShot = 100;
-                        int i;
-                        for (i = 0; i < 10; i++)
-                        {
-                            offsetAngle = (startAngle + deltaAngle * (i + i * i) / 2f) + 32f * i;
-                            Projectile.NewProjectile(value9.X, value9.Y, (float)(Math.Sin(offsetAngle) * 5f), (float)(Math.Cos(offsetAngle) * 5f), projectileShot, damage, 0f, Main.myPlayer, 0f, 0f);
-                            Projectile.NewProjectile(value9.X, value9.Y, (float)(-Math.Sin(offsetAngle) * 5f), (float)(-Math.Cos(offsetAngle) * 5f), projectileShot, damage, 0f, Main.myPlayer, 0f, 0f);
-                        }
-                    }
-                    if (phasechangetime == 80)
-                    {
-                        for (int k = 0; k < 3; k++)
-                        {
-                            float Speed = 9.25f;
-                            Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
-                            int damage = 28;
-                            int type = mod.ProjectileType("MegaArchorb");
-                            float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
-                            int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, Main.myPlayer);
-                            Main.projectile[num54].velocity.X += (float)Main.rand.Next(-20, 21) * 0.05f;
-                            Main.projectile[num54].velocity.Y += (float)Main.rand.Next(-20, 21) * 0.05f;
-                            Main.projectile[num54].netUpdate = true;
-                        }
-                    }
-                    if (phasechangetime == 160)
-                    {
-                        for (int k = 0; k < 3; k++)
-                        {
-                            float Speed = 9.25f;
-                            Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
-                            int damage = 28;
-                            int type = mod.ProjectileType("MegaArchorb");
-                            float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
-                            int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, Main.myPlayer);
-                            Main.projectile[num54].velocity.X += (float)Main.rand.Next(-20, 21) * 0.05f;
-                            Main.projectile[num54].velocity.Y += (float)Main.rand.Next(-20, 21) * 0.05f;
-                            Main.projectile[num54].netUpdate = true;
-                        }
-                    }
-                    if (phasechangetime == 240)
-                    {
-                        for (int k = 0; k < 3; k++)
-                        {
-                            float Speed = 9.25f;
-                            Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
-                            int damage = 28;
-                            int type = mod.ProjectileType("MegaArchorb");
-                            float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
-                            int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, Main.myPlayer);
-                            Main.projectile[num54].velocity.X += (float)Main.rand.Next(-20, 21) * 0.05f;
-                            Main.projectile[num54].velocity.Y += (float)Main.rand.Next(-20, 21) * 0.05f;
-                            Main.projectile[num54].netUpdate = true;
-                        }
-                    }
-                    if (phasechangetime >= 355)
-                    {
-                        if (Main.rand.Next(30) == 0)
-                        {
-                        }
-                        {
-                            float Speed = 1f + (Main.rand.Next(1, 8));
-                            Vector2 vector8 = new Vector2(npc.position.X + (npc.width / 2), npc.position.Y + (npc.height / 2));
-                            int damage = 20;
-                            int type = 38;
-                            float rotation = (float)Math.Atan2(vector8.Y - (P.position.Y + (P.height * 0.5f)), vector8.X - (P.position.X + (P.width * 0.5f)));
-                            int num54 = Projectile.NewProjectile(vector8.X, vector8.Y, (float)((Math.Cos(rotation) * Speed) * -1), (float)((Math.Sin(rotation) * Speed) * -1), type, damage, 0f, Main.myPlayer);
-                        }
-                    }
-                }
-            }
+					laserOrbTime = 0;
+				}
         }
         public override void FindFrame(int frameHeight)
         {
