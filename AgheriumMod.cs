b2061351484b16d2ca5be7defd797a119a7f6d57
Turquoise <reezzza.ai@gmail.com>
2018-04-09 15:51:09 -0700

    Add files via upload

--- AgheriumMod.cs
+++ AgheriumMod.cs
@@ -1,9 +1,24 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using AgheriumMod.NPCs.Bosses;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace AgheriumMod
 {
 	class AgheriumMod : Mod
 	{
+		public static bool downedSoul;
+		public static bool downedAngel;
+		public static bool downedRorbert;
+		public static bool downedSpodermen;
+		
 		public AgheriumMod()
 		{
 			Properties = new ModProperties()
@@ -12,6 +27,42 @@ namespace AgheriumMod
 				AutoloadGores = true,
 				AutoloadSounds = true
 			};
+		}
+		public override void PostSetupContent()
+		{
+			Mod bossList = ModLoader.GetMod("BossChecklist");
+			if (bossList != null)
+			{
+				bossList.Call("AddBossWithInfo", "Soul of the Guide", 0.001f, (Func<bool>)(() => downedSoul), string.Format("Use a [i:{0}]", ItemType("SoulActivator")));
+			}
+			else
+			{
+				
+			}
+			if (bossList != null)
+			{
+				bossList.Call("AddBossWithInfo", "Fallen Angel", 7.5f, (Func<bool>)(() => downedAngel), string.Format("Use a [i:{0}]", ItemType("UnholyBeacon")));
+			}
+			else
+			{
+				
+			}
+			if (bossList != null)
+			{
+				bossList.Call("AddBossWithInfo", "Rorbert", 5.1f, (Func<bool>)(() => downedRorbert), string.Format("Use a [i:{0}]", ItemType("StrangeMachine")));
+			}
+			else
+			{
+				
+			}
+			if (bossList != null)
+			{
+				bossList.Call("AddBossWithInfo", "Aarhac'n, the Spider Queen", 10.2f, (Func<bool>)(() => downedSpodermen), string.Format("Use a [i:{0}]", ItemType("SpiderEgg")));
+			}
+			else
+			{
+				
+			}
 		}
 	}
 }
