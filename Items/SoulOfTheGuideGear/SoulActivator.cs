457917ebfb6ac4a60c8d7ca528adcfb694218f30
Turquoise <reezzza.ai@gmail.com>
2018-04-09 15:52:16 -0700

    Add files via upload

---
+++ Items/SoulOfTheGuideGear/SoulActivator.cs
@@ -0,0 +1,57 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AgheriumMod.Items.SoulOfTheGuideGear
+{
+    public class SoulActivator : ModItem 
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Soul Activator"); 
+            Tooltip.SetDefault("Unleashes the Guide's soul energy"); 
+        }
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20; 
+            item.maxStack = 20; 
+            item.value = 0; 
+            item.rare = 7; 
+            item.useAnimation = 30; 
+            item.useTime = 30; 
+            item.useStyle = 4; 
+            item.consumable = false; 
+        }
+		public override bool CanUseItem(Player player)
+		{
+			int soulCount = NPC.CountNPCS(mod.NPCType("SoulOfTheGuide"));
+			if (soulCount >= 1)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+        public override bool UseItem(Player player)
+        {
+            NPC.SpawnOnPlayer(player.whoAmI, mod.NPCType("SoulOfTheGuide"));
+            Main.PlaySound(SoundID.Roar, player.position, 0);
+            return true;
+        }
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.IronBar, 7);
+            recipe.AddIngredient(ItemID.Lens, 1);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this, 1);
+			recipe.AddRecipe();
+		}
+    }
+}
