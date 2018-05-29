c278d5eb585e200e46dd9c481320c648b67a4b28
Turquoise <reezzza.ai@gmail.com>
2018-05-29 12:31:50 -0700

    Add files via upload
    core stuff

--- AgheriumMod.cs
+++ AgheriumMod.cs
@@ -28,7 +28,7 @@ namespace AgheriumMod
 			Mod bossList = ModLoader.GetMod("BossChecklist");
 			if (bossList != null)
 			{
-				bossList.Call("AddBossWithInfo", "Soul of the Guide", 0.001f, (Func<bool>)(() => AgheriumWorld.downedSoul), string.Format("Use a [i:{0}]", ItemType("SoulActivator")));
+				bossList.Call("AddBossWithInfo", "Soul of the Guide", 3.4f, (Func<bool>)(() => AgheriumWorld.downedSoul), string.Format("Use a [i:{0}]", ItemType("SoulActivator")));
 				bossList.Call("AddBossWithInfo", "Fallen Angel", 7.5f, (Func<bool>)(() => AgheriumWorld.downedAngel), string.Format("Use a [i:{0}]", ItemType("UnholyBeacon")));
 				bossList.Call("AddBossWithInfo", "Rorbert", 5.1f, (Func<bool>)(() => AgheriumWorld.downedRorbert), string.Format("Use a [i:{0}]", ItemType("StrangeMachine")));
 				bossList.Call("AddBossWithInfo", "Aarhac'n, the Spider Queen", 10.2f, (Func<bool>)(() => AgheriumWorld.downedSpodermen), string.Format("Use a [i:{0}]", ItemType("SpiderEgg")));
