383c5577982ff025fbf9670a19c8bb750155e934
Turquoise <reezzza.ai@gmail.com>
2018-06-03 12:58:20 -0400

    Add files via upload
    Agherium Shotbow "buff"

--- Items/AgheriumGear/AgheriumShotbow.cs
+++ Items/AgheriumGear/AgheriumShotbow.cs
@@ -5,11 +5,16 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using AgheriumMod;
+using AgheriumMod.Items;
+using AgheriumMod.Items.AgheriumGear;
 
 namespace AgheriumMod.Items.AgheriumGear
 {
     public class AgheriumShotbow : ModItem
     {
+		int arrowsShot;
+		int arrowType;
         public override void SetDefaults()
         {
             item.damage = 16;
@@ -32,16 +37,60 @@ namespace AgheriumMod.Items.AgheriumGear
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Agherium Shotbow");
-			Tooltip.SetDefault("Shoots six arrows at once! \nTsuki's weapon of choice. Might as well make this his dev item, even though he's not a developer.");
+			Tooltip.SetDefault("Shoots multiple arrows at once! \nTsuki's weapon of choice. Might as well make this his dev item, even though he's not a developer.");
         }
         public override bool Shoot(Player player, ref Microsoft.Xna.Framework.Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            int num6 = Main.rand.Next(2, 5);
-            for (int index = 0; index < num6; ++index)
+			if (AgheriumPlayer.ARROWS = true)
+			{
+				if (Main.hardMode != true)
+				{
+					arrowType = 1;
+					item.damage = 16;
+					arrowsShot = Main.rand.Next(2, 5);
+				}
+				if (NPC.downedBoss3 == true && Main.hardMode != true)
+				{
+					arrowType = 1;
+					item.damage = 21;
+				}
+				if (Main.hardMode == true && NPC.downedMechBoss1 != true && NPC.downedMechBoss2 != true && NPC.downedMechBoss3 != true)
+				{
+					arrowType = 103;
+					arrowsShot = Main.rand.Next(3, 7);
+					item.damage = 40;
+				}
+				if (NPC.downedMechBoss1 == true && NPC.downedMechBoss2 == true && NPC.downedMechBoss3 == true && NPC.downedGolemBoss != true)
+				{
+					arrowType = 103;
+					arrowsShot = Main.rand.Next(3, 7);
+					item.damage = 55;
+				}
+				if (NPC.downedGolemBoss == true && NPC.downedMoonlord != true)
+				{
+					arrowType = 103;
+					arrowsShot = Main.rand.Next(3, 7);
+					item.damage = 67;
+				}
+				if (NPC.downedMoonlord == true)
+				{
+					arrowType = 639;
+					arrowsShot = Main.rand.Next(3, 7);
+					item.damage = 81;
+				}
+			}
+			else if (AgheriumPlayer.ARROWS = false)
+			{
+				arrowType = 1;
+				item.damage = 16;
+				arrowsShot = Main.rand.Next(2, 5);
+			}
+            
+            for (int index = 0; index < arrowsShot; ++index)
             {
                 float SpeedX = speedX + (float)Main.rand.Next(-10, 11) * 0.045f;
                 float SpeedY = speedY + (float)Main.rand.Next(-10, 11) * 0.045f;
-                int projectile1 = Projectile.NewProjectile(position.X, position.Y, SpeedX, SpeedY, type, damage, knockBack, player.whoAmI, 0.0f, 0.5f + (float)Main.rand.NextDouble() * 0.9f);
+                int projectile1 = Projectile.NewProjectile(position.X, position.Y, SpeedX, SpeedY, arrowType, damage, knockBack, player.whoAmI, 0.0f, 0.5f + (float)Main.rand.NextDouble() * 0.9f);
             }
             return false;
         }
