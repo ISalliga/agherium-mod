afc1cbfef91c93b939e8f099477d85715b2a4e71
Turquoise <reezzza.ai@gmail.com>
2018-06-09 20:18:58 -0400

    Add files via upload
    misc gear full thing cuz i cba to upload the files 1 by 1...

--- Items/MiscGear/Biginator.cs
+++ Items/MiscGear/Biginator.cs
@@ -9,22 +9,22 @@ using AgheriumMod.Items;
 
 namespace AgheriumMod.Items.MiscGear
 {
-    public class Smallinator : ModItem
+    public class Biginator : ModItem
     {
 		public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Smallerizer");
-            Tooltip.SetDefault("Shrinks enemies and, as long as the enemy's defense isn't below five, cuts their defense down");
+            DisplayName.SetDefault("Bigifier");
+            Tooltip.SetDefault("Makes enemies larger and deals good amounts of damage");
         }
         public override void SetDefaults()
         {
-            item.damage = 1;
+            item.damage = 75;
             item.ranged = true;
             item.width = 60;
             item.height = 40;
             item.useTime = 34;
             item.useAnimation = 34;
-			item.UseSound = new Terraria.Audio.LegacySoundStyle(2, 114);
+			item.UseSound = new Terraria.Audio.LegacySoundStyle(2, 115);
             item.useStyle = 5;
             item.noMelee = true; //so the item's animation doesn't do damage
             item.knockBack = 7f;
@@ -32,12 +32,12 @@ namespace AgheriumMod.Items.MiscGear
             item.rare = 10;
             item.autoReuse = true;
             item.shootSpeed = 25f;
-            item.shoot = mod.ProjectileType("SmallThing");
+            item.shoot = mod.ProjectileType("BigThing");
         }
 
         public override bool Shoot(Player player, ref Microsoft.Xna.Framework.Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, mod.ProjectileType("SmallThing"), damage, knockBack, player.whoAmI, 0.0f, 0.0f);
+            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, mod.ProjectileType("BigThing"), damage, knockBack, player.whoAmI, 0.0f, 0.0f);
             return false;
         }
     }
