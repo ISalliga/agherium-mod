992a80aaef910b7b59ed4fa78cbb4dba11925af3
Turquoise <reezzza.ai@gmail.com>
2018-06-16 14:46:02 -0400

    Add files via upload
    main folder stuffs

--- AgheriumPlayer.cs
+++ AgheriumPlayer.cs
@@ -22,14 +22,19 @@ namespace AgheriumMod
 		public bool taxonGreaves = false;
 		public bool taxonSetBonus = false;
 		public bool taxonSetBonus2 = false;
+		public bool trydanSetBonus = false;
 		public bool ARROWS = false;
 		public bool isFuryBeingForged = false;
+		public bool trydanEmblem = false;
+		
 		public override void ResetEffects()
 		{
 			isFuryBeingForged = false;
 			taxonGreaves = false;
 			taxonSetBonus = false;
 			taxonSetBonus2 = false;
+			trydanSetBonus = false;
+			trydanEmblem = false;
 		}
 		public override void Hurt(bool pvp, bool quiet, double damage, int hitDirection, bool crit)
 		{
@@ -82,6 +87,10 @@ namespace AgheriumMod
 					player.AddBuff(mod.BuffType("TaxonDefenseBoost"), 240);
 				}
 			}
+			if (trydanEmblem == true)
+			{
+				damage += Main.rand.Next(1, 5);
+			}
 		}
 		public override void OnHitNPCWithProj(Projectile proj, NPC target, int damage, float knockback, bool crit)
 		{
@@ -98,6 +107,32 @@ namespace AgheriumMod
 				{
 					player.AddBuff(mod.BuffType("TaxonDefenseBoost"), 240);
 				}
+			}
+			if (trydanSetBonus && Main.rand.Next(1, 6) == 1 && proj.melee != true)
+			{
+				for (int projerino = 0; projerino <= 5; projerino++)
+				{
+					int projLel = Projectile.NewProjectile(target.Center.X, target.Center.Y, Main.rand.Next(-8, 8), Main.rand.Next(-7, 8), mod.ProjectileType("MiniBeamProj"), 1, 0, Main.myPlayer, 0.0f, 0.5f + (float)Main.rand.NextDouble() * 0.9f);
+					Main.projectile[projLel].magic = false;
+					Main.projectile[projLel].melee = true;
+					Main.projectile[projLel].penetrate = -1;
+					Main.projectile[projLel].timeLeft = 60;
+				}
+			}
+			if (trydanEmblem)
+			{
+				damage += Main.rand.Next(1, 5);
+			}
+		}
+		public override void PostUpdateRunSpeeds()
+		{
+			if (trydanSetBonus)
+			{
+				player.maxRunSpeed *= 2;
+			}
+			if (taxonGreaves)
+			{
+				player.maxRunSpeed *= (int)1.3f;
 			}
 		}
 	}
